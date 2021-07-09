3a1196d63a419178f27f212b368ed50bb34f6e08
Arniel Casile <arniel.casile@fujitsu.com>
2021-07-09 14:10:27 +0800

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,63 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <Header :numCorrect="numCorrect" :numTotal="numTotal" />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox
+            v-if="questions.length"
+            :currentQuestion="questions[index]"
+            :next="next"
+            :increment="increment"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
+
+<script>
+import Header from "./components/Header.vue";
+import QuestionBox from "./components/QuestionBox.vue";
+
+export default {
+  name: "app",
+  components: {
+    Header,
+    QuestionBox,
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0,
+    };
+  },
+  methods: {
+    next() {
+      this.index++;
+    },
+    increment(isCorrect) {
+      if (isCorrect) {
+        this.numCorrect++;
+      }
+      this.numTotal++;
+    },
+  },
+  mounted: function () {
+    fetch("https://opentdb.com/api.php?amount=10&category=27&type=multiple", {
+      method: "get",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonData) => {
+        this.questions = jsonData.results;
+      });
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -15,18 +66,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
